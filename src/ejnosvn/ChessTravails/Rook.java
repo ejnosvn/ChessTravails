6c7ba32ab5dddd9291a8ff6d3c2686468afa69f6
eandrks <andras.rokas@ericsson.com>
2014-09-25 09:04:51 +0200

    readability refactor

--- src/ejnosvn/ChessTravails/Rook.java
+++ src/ejnosvn/ChessTravails/Rook.java
@@ -12,28 +12,14 @@ public class Rook extends Chessman {
 	@Override
 	public List<Field> getValidMoves(int startCol, int startRow) {
 		List<Field> retFields = new ArrayList<Field>();
-		for (int col = startCol + 1; col < 8; col++) {
-			if (t.getField(col, startRow).isValid()) {
-				retFields.add(t.getField(col, startRow));
-			} else {
-				break;
-			}
-		}
-		for (int col = startCol - 1; col >= 0; col--) {
-			if (t.getField(col, startRow).isValid()) {
-				retFields.add(t.getField(col, startRow));
-			} else {
-				break;
-			}
-		}
+		checkValidMovesRight(startCol, startRow, retFields);
+		checkValidMovesLeft(startCol, startRow, retFields);
+		checkValidMovesDown(startCol, startRow, retFields);
+		checkValidMovesUp(startCol, startRow, retFields);
+		return retFields;
+	}
 
-		for (int row = startRow + 1; row < 8; row++) {
-			if (t.getField(startCol, row).isValid()) {
-				retFields.add(t.getField(startCol, row));
-			} else {
-				break;
-			}
-		}
+	private void checkValidMovesUp(int startCol, int startRow, List<Field> retFields) {
 		for (int row = startRow - 1; row >= 0; row--) {
 			if (t.getField(startCol, row).isValid()) {
 				retFields.add(t.getField(startCol, row));
@@ -41,34 +27,49 @@ public class Rook extends Chessman {
 				break;
 			}
 		}
-		return retFields;
+	}
+
+	private void checkValidMovesDown(int startCol, int startRow, List<Field> retFields) {
+		for (int row = startRow + 1; row < 8; row++) {
+			if (t.getField(startCol, row).isValid()) {
+				retFields.add(t.getField(startCol, row));
+			} else {
+				break;
+			}
+		}
+	}
+
+	private void checkValidMovesLeft(int startCol, int startRow, List<Field> retFields) {
+		for (int col = startCol - 1; col >= 0; col--) {
+			if (t.getField(col, startRow).isValid()) {
+				retFields.add(t.getField(col, startRow));
+			} else {
+				break;
+			}
+		}
+	}
+
+	private void checkValidMovesRight(int startCol, int startRow, List<Field> retFields) {
+		for (int col = startCol + 1; col < 8; col++) {
+			if (t.getField(col, startRow).isValid()) {
+				retFields.add(t.getField(col, startRow));
+			} else {
+				break;
+			}
+		}
 	}
 
 	@Override
 	public List<Field> getNotBlockedMoves(int startCol, int startRow) {
 		List<Field> retFields = new ArrayList<Field>();
-		for (int col = startCol + 1; col < 8; col++) {
-			if (t.getField(col, startRow).isNotBlocked()) {
-				retFields.add(t.getField(col, startRow));
-			} else {
-				break;
-			}
-		}
-		for (int col = startCol - 1; col >= 0; col--) {
-			if (t.getField(col, startRow).isNotBlocked()) {
-				retFields.add(t.getField(col, startRow));
-			} else {
-				break;
-			}
-		}
+		checkNotBlockedMovesRight(startCol, startRow, retFields);
+		checkNotBlockedMovesLeft(startCol, startRow, retFields);
+		checkNotBlockedMovesDown(startCol, startRow, retFields);
+		checkNotBlockedMovesUp(startCol, startRow, retFields);
+		return retFields;
+	}
 
-		for (int row = startRow + 1; row < 8; row++) {
-			if (t.getField(startCol, row).isNotBlocked()) {
-				retFields.add(t.getField(startCol, row));
-			} else {
-				break;
-			}
-		}
+	private void checkNotBlockedMovesUp(int startCol, int startRow, List<Field> retFields) {
 		for (int row = startRow - 1; row >= 0; row--) {
 			if (t.getField(startCol, row).isNotBlocked()) {
 				retFields.add(t.getField(startCol, row));
@@ -76,9 +77,36 @@ public class Rook extends Chessman {
 				break;
 			}
 		}
+	}
 
-		//		System.out.println("GetValidMoves for Rook:"+ t.getField(startCol,startRow).toString() + " :" + retFields.toString());
-		return retFields;
+	private void checkNotBlockedMovesDown(int startCol, int startRow, List<Field> retFields) {
+		for (int row = startRow + 1; row < 8; row++) {
+			if (t.getField(startCol, row).isNotBlocked()) {
+				retFields.add(t.getField(startCol, row));
+			} else {
+				break;
+			}
+		}
+	}
+
+	private void checkNotBlockedMovesLeft(int startCol, int startRow, List<Field> retFields) {
+		for (int col = startCol - 1; col >= 0; col--) {
+			if (t.getField(col, startRow).isNotBlocked()) {
+				retFields.add(t.getField(col, startRow));
+			} else {
+				break;
+			}
+		}
+	}
+
+	private void checkNotBlockedMovesRight(int startCol, int startRow, List<Field> retFields) {
+		for (int col = startCol + 1; col < 8; col++) {
+			if (t.getField(col, startRow).isNotBlocked()) {
+				retFields.add(t.getField(col, startRow));
+			} else {
+				break;
+			}
+		}
 	}
 
 }
